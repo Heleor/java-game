58e23473ed8de8f411782f9394a1ee7b9ae5788f
Andrei <Andrei@MainPC>
2016-01-09 20:15:14 -0800

    * libgdx integration

---
+++ desktop/src/personal/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package personal.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import personal.game.TestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestGame(), config);
+	}
+}
