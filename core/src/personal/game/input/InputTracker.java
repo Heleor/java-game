4b344121c6ccd2e461b632da72fd3ac1dfa38fe1
Andrei <Andrei@MainPC>
2016-03-06 11:28:39 -0800

    Preventing memory leak in input tracker.

--- core/src/personal/game/input/InputTracker.java
+++ core/src/personal/game/input/InputTracker.java
@@ -1,18 +1,29 @@
 package personal.game.input;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputTracker {
+	private static final int BUFFER_SIZE = 60 * 5;
 	public static InputTracker singleton = new InputTracker();
 	
+	private List<InputFrame> previousFrame;
 	public List<InputFrame> history;
 	
 	private InputTracker() {
-		history = new LinkedList<>();
+		history = new ArrayList<>(BUFFER_SIZE);
 	}
 	
 	public void addFrame(InputFrame frame) {
 		history.add(frame);
+		if (history.size() > BUFFER_SIZE) {
+			previousFrame = history;
+			history = new ArrayList<>(BUFFER_SIZE);
+			drain(previousFrame);
+		}
+	}
+	
+	private void drain(List<InputFrame> frame) {
+		// Throw it away for now.
 	}
 }
